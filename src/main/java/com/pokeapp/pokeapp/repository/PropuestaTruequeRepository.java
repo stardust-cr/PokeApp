1bc4b8ab93c5d0397dd0bd43950d9361eb59ecb5
conchi <correodeconchir@gmail.com>
2026-05-24 23:36:09 +0200

    solucion

---
+++ src/main/java/com/pokeapp/pokeapp/repository/PropuestaTruequeRepository.java
@@ -0,0 +1,24 @@
+package com.pokeapp.pokeapp.repository;
+
+import com.pokeapp.pokeapp.model.PropuestaTrueque;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PropuestaTruequeRepository extends JpaRepository<PropuestaTrueque, Long> {
+
+    // Todas las propuestas sobre una venta (para el vendedor)
+    List<PropuestaTrueque> findByVentaId(Long ventaId);
+
+    // Propuestas enviadas por un usuario
+    List<PropuestaTrueque> findByProponenteId(Long proponenteId);
+
+    // Propuestas pendientes recibidas para las ventas de un vendedor
+    List<PropuestaTrueque> findByVenta_Vendedor_IdAndEstado(Long vendedorId, PropuestaTrueque.EstadoPropuesta estado);
+
+    // Contar pendientes para el badge de notificaciones
+    long countByVenta_Vendedor_IdAndEstado(Long vendedorId, PropuestaTrueque.EstadoPropuesta estado);
+}
+
